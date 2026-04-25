3d49779d29d7ff2ffca5af4bcba5fd6e5d5a7126
Hossein_dev81 <hszhosalei81@gmail.com>
2026-04-25 07:15:50 +0330

    feat(accounts): implement user registration serializer with validation

---
+++ account/api/v1/serializers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+
+
+
+User = get_user_model()
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password1=serializers.CharFeild(max_length=255,write_only=True)
+    class Meta:
+        model = User
+        fields = ('email','password','password1')
+
+    def validate(self, kwargs):
+        password=kwargs['password']
+        password1=kwargs['password1']
+        if password!=password1:
+            raise serializers.ValidationError('Password does not match')
+        try:
+            validate_password(password)
+
+        except ValidationError as e:
+            raise serializers.ValidationError({"password": list(e.messages)})
+        return kwargs
+
+
+    def create(self, validated_data):
+        validated_data.pop('password1')
+        return User.objects.create_user(**validated_data)
+
