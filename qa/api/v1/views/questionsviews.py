ab189120ec6426431602651087c5c37c2a8aa1c1
Hossein_dev81 <hszhosalei81@gmail.com>
2026-04-30 19:00:07 +0330

    feat(qa): add search and pagination support for Question API

--- qa/api/v1/views/questionsviews.py
+++ qa/api/v1/views/questionsviews.py
@@ -3,12 +3,14 @@ from rest_framework.permissions import IsAuthenticated
 from ....permissions import IsActivatedUser,IsAuthorUser
 from ..serializers.questions_serializers import QuestionSerializer
 from ....models.question import Question
+from rest_framework.filters import SearchFilter
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     permission_classes = [IsAuthenticated,IsActivatedUser,IsAuthorUser]
-
+    filter_backends = [SearchFilter]
+    search_fields = ['question_text']
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
