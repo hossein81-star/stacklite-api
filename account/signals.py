85ed05908ed957238db6cbf8e059b373895b07bc
Hossein_dev81 <hszhosalei81@gmail.com>
2026-04-30 09:49:42 +0330

    fix(accounts): resolve profile update bug causing validation failure

--- account/signals.py
+++ account/signals.py
@@ -10,4 +10,4 @@ User = get_user_model()
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance,username=instance.email)
